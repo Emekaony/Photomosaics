0664d429efea6b430be516d7fb07f675b5a85488
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-09-20 02:20:23 -0700

    Used smaller image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("This is the index file");
+import { Image } from "image-js";
+
+// function to laod the image asynchronously
+(async function laodImage() {
+  try {
+    let img = await Image.load("images/example_input.jpg");
+    console.log("Image width is: ", img.width);
+    console.log("Image height is: ", img.height);
+  } catch (e) {
+    console.log("error loading the image: ", e);
+  }
+})();
