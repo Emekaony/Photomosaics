a230ad0ca7de110a22bc6cf8d569a17a8ff5ac93
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-09-26 13:25:20 -0700

    No big change

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Image } from "image-js";
 
 // function to laod the image asynchronously
-(async function loadImage() {
+async function loadImage() {
   try {
-    let img = await Image.load("images/example_input.jpg");
+    let img: Image = await Image.load("images/example_input.jpg");
     console.log("Image width is: ", img.width);
     console.log("Image height is: ", img.height);
   } catch (e) {
     console.log("error loading the image: ", e);
   }
-})();
+}
+console.log("I like how this project just works with typescript out the box!");
+loadImage();
