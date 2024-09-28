43819efdc090b300bde39ad4804e427b3620dac3
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-09-27 22:58:38 -0700

    I set up the ts project properly now and everything runs fine.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Image } from "image-js";
 
 // function to laod the image asynchronously
-async function loadImage() {
+(async function loadImage() {
   try {
     let img: Image = await Image.load("images/example_input.jpg");
     console.log("Image width is: ", img.width);
@@ -9,6 +9,4 @@ async function loadImage() {
   } catch (e) {
     console.log("error loading the image: ", e);
   }
-}
-console.log("I like how this project just works with typescript out the box!");
-loadImage();
+})();
