ca704b797d1722e73238a6f77390fb9b8cdc64a5
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-09-29 01:04:16 -0700

    Running apps thru the main function from now on.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import { Image } from "image-js";
 
 // function to laod the image asynchronously
-(async function loadImage() {
+async function loadImage() {
   try {
     let img: Image = await Image.load("images/example_input.jpg");
+    console.log(img.width);
   } catch (e) {
     console.log("error loading the image: ", e);
   }
+}
+
+(function main() {
+  loadImage();
 })();
